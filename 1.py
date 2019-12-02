abcb280f7655ce033cb70d2d4730943bb1d48a73
Tomas Trnka <trnkat@avast.com>
2019-12-02 10:15:28 +0100

    final version of day 1

--- 1.py
+++ 1.py
@@ -8,6 +8,16 @@ args = parser.parse_args()
 dataset = tf.data.TextLineDataset(args.file)
 dataset = dataset.map(lambda x: tf.strings.to_number(x, tf.float32))
 
-dataset_pre_line_transform = dataset.map(lambda x: tf.math.subtract(tf.math.floor(tf.divide(x,3.0)), 2.0))
+@tf.function
+def compute_fuel(x):
+    fuel_mass = tf.math.subtract(tf.math.floor(tf.divide(x,3.0)), 2.0)
+    fuel_mass_temp = tf.identity(fuel_mass)
+    while fuel_mass_temp > 0.0:        
+        fuel_mass_temp = tf.math.maximum(0.0, tf.math.subtract(tf.math.floor(tf.divide(fuel_mass_temp,3.0)), 2.0))        
+        fuel_mass = tf.add(fuel_mass, fuel_mass_temp)
+    return fuel_mass
 
-print(dataset_pre_line_transform.reduce(0.0, lambda state, value: state + value).numpy())
+print(compute_fuel(1969))
+
+dataset_pre_line_transform_fuel_for_fuel = dataset.map(lambda x: compute_fuel(x))
+print(dataset_pre_line_transform_fuel_for_fuel.reduce(0.0, lambda state, value: state + value).numpy())
